ac219f8718686b70074e672d18e9bd80bdc5da17
roshan-githubb <roshanbc9860@gmail.com>
2023-08-30 18:18:22 +0545

    Initial commit

---
+++ src/main/java/Macys/com/macysSelenium/App.java
@@ -0,0 +1,13 @@
+package Macys.com.macysSelenium;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
